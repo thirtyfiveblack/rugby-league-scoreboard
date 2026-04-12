4b56f4139886e1fe844986ab013aa333ee8b0adf
Andrew Howard <thirtyfiveblack@gmail.com>
2026-04-12 11:11:55 +1000

    Update sports.py

--- sports.py
+++ sports.py
@@ -1082,7 +1082,7 @@ class SportsCore(ABC):
                 "home_id": home_team["id"],
                 "home_score": home_score,
                 "home_logo_path": self.logo_dir
-                / Path(f"{LogoDownloader.(home_id)}.png"),
+                / Path(f"{home_id}.png"),
                 "home_logo_url": home_logo_url,
                 "home_record": home_record,
                 "away_record": away_record,
@@ -1090,7 +1090,7 @@ class SportsCore(ABC):
                 "away_id": away_team["id"],
                 "away_score": away_score,
                 "away_logo_path": self.logo_dir
-                / Path(f"{LogoDownloader.(away_id)}.png"),
+                / Path(f"{away_id}.png"),
                 "away_logo_url": away_logo_url,
                 "is_within_window": True,  # Whether game is within display window
             }
